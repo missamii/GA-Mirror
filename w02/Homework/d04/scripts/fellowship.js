d9008776f8637a5a3e85fc0b167ce4cd3ee21176
Ethan Friedman <ethanjfriedman@gmail.com>
2016-06-23 13:45:10 -0400

    w02d04 hw

---
+++ w02/Homework/d04/scripts/fellowship.js
@@ -0,0 +1,24 @@
+console.log("W01D04 -- fellowship.js linked.");
+
+// Our heroes
+var hobbits = [
+  'Frodo Baggins',
+  'Samwise \'Sam\' Gamgee',
+  'Meriadoc \'Merry\' Brandybuck',
+  'Peregrin \'Pippin\' Took'
+];
+
+// Their compatriots
+var buddies = [
+  'Gandalf the Grey',
+  'Legolas',
+  'Gimli',
+  'Strider',
+  'Boromir'
+];
+
+// Their journey
+var lands = ['The Shire', 'Rivendell', 'Mordor'];
+
+// An example of using query selector
+var body = document.querySelector('body');
