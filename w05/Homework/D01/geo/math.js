71d36c1dadf5940579a95275611f39ec0c1af8be
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-07-11 23:07:32 -0400

    completed math js hw

--- w05/Homework/D01/geo/math.js
+++ w05/Homework/D01/geo/math.js
@@ -1,14 +1,13 @@
 var mathmatics = require('./geometry.js');
 
-console.log('im mathmatice!!!', mathmatics);
+console.log('im mathmatics!!!', mathmatics);
 
   if (process.argv[2] === 'circle') {
     console.log("Answer!!!: ", mathmatics.circle(process.argv[3]));
   }
     else(process.argv[3] === 'rectangle')
-    console.log("Answer!!!: ", mathmatics.rectangle(process.argv[3]));
+    console.log("Answer!!!: ", mathmatics.rectangle(process.argv[3],process.argv[4]));
   }
     else if (process.argv[1] === 'square') {
       console.log("Answer!!!: ", mathmatics.square(process.argv[3]));
-
-    }
+    };
