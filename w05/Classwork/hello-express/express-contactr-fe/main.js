38dc1414750d605f8e30a333d53fa2a05049bd5d
Syed Salahudin <syedamin@cs.nyu.edu>
2016-07-12 13:21:23 -0400

    some updates

--- w05/Classwork/hello-express/express-contactr-fe/main.js
+++ w05/Classwork/hello-express/express-contactr-fe/main.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
-  console.log("here");
-  var contact = { name: "syed" };
-  $.post('http://localhost:3000/', contact ,function(res){
-    console.log(res);
+  /*
+  $.get('http://localhost:3000/', function(responseFromServer){
+    console.log(responseFromServer);
+  });
+  */
+  var name = prompt('enter a name');
+  var contact = {};
+  contact['name'] = name;
+  console.log(contact);
+  $.post('http://localhost:3000/', contact, function(responseFromServer){
+    console.log(responseFromServer);
   });
 });
--- w05/Classwork/hello-express/express-contactr/index.js
+++ w05/Classwork/hello-express/express-contactr/index.js
@@ -1,26 +1,36 @@
 var express = require('express');
 var cors = require('cors');
-var bodyParser = require('body-parser');
+var bp = require('body-parser');
 var fs = require('fs');
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(cors());
+app.use(bp.urlencoded({extended: true}));
+app.use(bp.json());
 
 app.get('/', function(req, res){
-  console.log("get request");
-
+  console.log("this is get request");
+  fs.readFile('contacts.json', function(err, data){
+    var contacts = JSON.parse(data);
+    res.send(contacts);
+  });
 });
 
 app.post('/', function(req, res){
-  console.log("post request");
+  console.log("this is a post request");
   console.log(req.body);
-  fs.writeFile('contacts.json', JSON.stringify(req.body), function(err, data){
-    console.log("data writ");
+  fs.readFile('contacts.json', function(err, data){
+    var currentContacts = JSON.parse(data);
+    currentContacts.push(req.body);
+    console.log("this is the thing: "+ currentContacts)
+    fs.writeFile('contacts.json', JSON.stringify(currentContacts), function(err, data){
+      console.log("data has been written");
+      res.send({contacts: currentContacts});
+    });
   });
 });
 
 
 app.listen(3000, function(){
-  console.log("express server started");
+  console.log('server is running');
 });
