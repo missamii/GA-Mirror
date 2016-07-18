7fa726b37b4f9b3ac6b59543afc1636556e41534
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-07-18 00:01:33 -0400

    i put the css till last worked on js straight thru. wish i was able to make the css nicer. time for sleep.

---
+++ w05/Homework/marvel/backend/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cors = require('cors');
+// var fs = require('fs');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+var app = express();
+
+// json
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect to the database server
+var MongoClient = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/marvel'
+mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  db = database;
+  console.log("Database connection ready");
+  var server = app.listen(process.env.PORT || 3000, function () {
+    var port = server.address().port;
+    console.log("App is running on port", port);
+  });
+});//end mongo client
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
---
+++ w05/Homework/marvel/frontend/main.js
@@ -0,0 +1,67 @@
+window.onload = function() {
+
+//get all the elements
+var div = document.querySelector('#box');
+var searchButton = document.querySelector('#searchbutton');
+var search = document.querySelector('#searchbox');
+var newdiv = document.querySelector('#newdiv');
+
+// hide upon load:
+  // newdiv.style.display = 'none';
+
+  /* Submit button */
+  searchButton.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    console.log(search.value);
+
+    var drop = document.querySelector('#dropbox');
+    console.log(drop);
+    var searchValue = search.value;
+    var chosenEndPoint = drop.value;
+    console.log(chosenEndPoint);
+    var queryStr = '';
+    if (chosenEndPoint === 'characters') {
+      queryStr = '?name=';
+    } else if (chosenEndPoint === 'comics') {
+      queryStr = '?title=';
+    }
+    queryStr += searchValue;
+    console.log(queryStr);
+    var endPoint = 'http://gateway.marvel.com:80/v1/public/' + chosenEndPoint;
+    var marvelApiKey = '&apikey=' + MARVEL_PUBLIC_API_KEY;
+    var url = endPoint + queryStr + marvelApiKey;
+    console.log(url);
+
+      //ajax stuff
+        $.ajax({
+          url: url,
+          dataType: 'json'
+        }).done(function(response){
+          console.log("response: ", response);
+          // appending API info to the DOM
+          var html = '';
+          if (chosenEndPoint === 'characters') {
+            for (var i = 0; i < response.data.results.length; i++) {
+              var character = response.data.results[i];
+              html += '<h3>' + character.name + '</h3>';
+              html += '<p>id: ' + character.id + '</p>';
+              html += '<img src="'+character.thumbnail.path + '.' + character.thumbnail.extension + '">';
+              html += '<p>' + character.description + '</p>';
+            }
+            html += response.attributionHTML;
+          } else if (chosenEndPoint === 'comics') {
+            for (var i = 0; i < response.data.results.length; i++) {
+              var comic = response.data.results[i];
+              html += '<h3>' + comic.title + '</h3>';
+              html += '<p>id: ' + comic.id + '</p>';
+              html += '<p>page count: ' + comic.pageCount + '</p>';
+              html += '<p>price: $' + comic.prices[0].price + '</p>';
+              html += '<img src="'+comic.thumbnail.path + '.' + comic.thumbnail.extension + '">';
+              html += '<p>' + comic.description + '</p>';
+            }
+            html += response.attributionHTML;
+          }
+          document.querySelector('#newdiv').innerHTML = html;
+        })
+      })
+}
