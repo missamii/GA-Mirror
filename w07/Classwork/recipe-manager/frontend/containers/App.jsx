b1267271450d9c5c2bf230222c76b94dc0de4064
Ethan Friedman <ethanjfriedman@gmail.com>
2016-07-27 14:35:38 -0400

    refactoring into subdirectories

--- w07/Classwork/recipe-manager/frontend/containers/App.jsx
+++ w07/Classwork/recipe-manager/frontend/containers/App.jsx
@@ -1,9 +1,9 @@
 var React = require('react');
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import AppStyles from './stylesheets/App.js';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+import AppStyles from '../stylesheets/App.js';
 import RecipesDisplay from './RecipesDisplay.jsx';
-import recipes from './data/recipes.js';
+import recipes from '../data/recipes.js';
 
 var App = React.createClass({
   render: function() {
