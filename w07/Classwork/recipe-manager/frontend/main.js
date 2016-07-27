8da20c6c7bde1e620be4466e7e10ed33fb85d49b
Ethan Friedman <ethanjfriedman@gmail.com>
2016-07-27 14:36:06 -0400

    refactoring into subdirectories part 2

---
+++ w07/Classwork/recipe-manager/frontend/main.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+import AppComponent from './containers/App.jsx';
+
+ReactDOM.render(
+  <AppComponent />,
+  document.getElementById('react-app')
+);
