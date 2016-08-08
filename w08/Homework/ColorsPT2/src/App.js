4e8d21f669f5825d00dc1d41b1a11e6d1042ff19
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-08-07 23:54:14 -0400

    styling but logic works

--- w08/Homework/ColorsPT2/src/App.js
+++ w08/Homework/ColorsPT2/src/App.js
@@ -1,37 +1,76 @@
 import React, { Component } from 'react';
-import Helper from './utils/Helpers.js';
+// import Helpers from './utils/Helpers.js';
+import fetchJsonp from 'fetch-jsonp';
 import './App.css';
+
+window.fj = fetchJsonp;
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      results: []
+      colors: [],
+      results: [],
+      listyle: []
     };
   }
   /* the event button for colors api */
   getColors(event) {
-    console.log(this);
-    getColors()
-    .then((res) => {
-      this.setState({
-        response: res.data
-      });
-      console.log(res.data);
+    event.preventDefault();
+
+    this.setState({
+      isFetching: true
     });
+
+    // fetch('http://www.colourlovers.com/api/palettes/random?format=json&jsonCallback=callback', {
+    fetchJsonp('http://www.colourlovers.com/api/palettes/random?format=json&jsonCallback=callback', {
+      jsonpCallbackFunction: 'callback'
+    }).then(response => response.json()).then((response) => {
+      console.log('response:', response);
+      this.setState({
+        isFetching: false,
+        colors: response[0].colors
+      });
+    }).catch(error => {
+      this.setState({
+        isFetching: false
+      });
+    });
+    // console.log(this);
+    // Helpers()
+    // .then((res) => {
+    //   this.setState({
+    //     response: res.data
+    //   });
+    //   console.log(res.data);
+    // });
   }
 
   render() {
+    const {
+      colors,
+      isFetching
+    } = this.state;
+
     return (
       <div id="box" className="App">
+        <h1>Colors YAY!</h1>
         <div id="color-container">
-          <h1>Colors YAY!</h1>
-         <br /> <br /><br /> <br />
-         <p>
-           Color display palette will go here
-         </p>
+          {colors.map((color, index) => (
+            <div
+              key={index}
+              className="color"
+              style={{
+                backgroundColor: `#${color}`
+              }} />
+          ))}
         </div>
-        <button id="button" onClick={(event) => this.getColors(event)}>Get Colors!</button>
+        <button
+          id="button"
+          disabled={isFetching}
+          onClick={(event) => this.getColors(event)}>
+          Get Colors!
+        </button>
       </div>
     );
   }
