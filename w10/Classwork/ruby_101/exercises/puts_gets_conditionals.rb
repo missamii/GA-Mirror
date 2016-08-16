44f6812fd33888b6725fd0e24b8af10e37345720
Liza Ramo <liza.h.ramo@gmail.com>
2016-08-16 11:59:34 -0400

    puts gets conditionals

---
+++ w10/Classwork/ruby_101/exercises/puts_gets_conditionals.rb
@@ -0,0 +1,15 @@
+
+# Ruby conditionals and puts & gets
+
+# Do these in a scratch '.rb' file & test them out in pry
+# Use PUTS & GETS
+
+
+# Exercises:
+# ---------------------------------- #
+
+
+# - Write a little program that asks the user what their name is. If their name is your name, puts "we have the same name!", otherwise puts "Your name is: <string interpolate their name here>"
+
+
+# - Write a little program that asks the user what week of WDI they're in. If they're below week 6, tell them they have a long way to go. If they're in week 6, tell them something encouraging. If they're over week 6, tell them they're almost there! 
