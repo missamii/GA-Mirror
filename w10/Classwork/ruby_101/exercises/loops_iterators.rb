e94df27baf66c883ddd5773c3bbc8daf83505c6a
Liza Ramo <liza.h.ramo@gmail.com>
2016-08-16 13:49:43 -0400

    loops iter

---
+++ w10/Classwork/ruby_101/exercises/loops_iterators.rb
@@ -0,0 +1,23 @@
+
+# Ruby loops & iterators
+
+# Exercise:
+# ---------------------------------- #
+
+num_arr = [1,2,3]
+
+num_hash = {
+  purple_rain: 22,
+  pangolins: 29,
+  robots: 24
+}
+
+# - `each` over num_arr and puts each element
+
+# - `each with index` over num_arr & puts each el & its index
+
+# - `map` over num_arr, adding 2 to each element
+
+# - 'each' over num_hash & puts each key/val pair as a single entity
+
+# - 'each' over num_hash & puts each key/val pair separately
