a407e3b66f4c836d63f12ed2aea9443f2b84d884
Liza Ramo <liza.h.ramo@gmail.com>
2016-08-16 14:25:20 -0400

    built in bangs

---
+++ w10/Classwork/ruby_101/exercises/built_in_bang_methods.rb
@@ -0,0 +1,9 @@
+# Built-in ruby methods & bang! methods
+
+
+# Exercise:
+# ---------------------------------- #
+
+# - Come up with examples that use at least 3 built-in string, array and hash methods (9 altogether)
+
+# - Use at least two bang (!) methods on strings or arrays
