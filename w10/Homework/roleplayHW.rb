0dcf3eb70641bc22b4c352316da39c3db3595bf3
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-08-18 22:21:29 -0400

    must sleep

--- w10/Homework/roleplayHW.rb
+++ w10/Homework/roleplayHW.rb
@@ -1,11 +1,11 @@
-# I will prompt a question to start the game, puts ""
-
-class Being
+class Being #the first class starts it all off!
+  #a list of symbols
   attr_accessor :spieces, :gender, :level, :hp, :max_hp, :current_hp
 
   @@count = 0
 
   def initialize( opts = {} )
+    # variables start here
     @spieces = opts[:spieces]
     @gender = opts[:gender]
     @level = opts[:level] || 1
@@ -15,13 +15,13 @@ class Being
     @@count += 1
     @id = @@count
     puts opts
-end
+  end #end of the ruby function
 
 def self.count
    return @@count
  end
 
- def take_damage(hp)
+ def damage(hp) #another function
     @current_hp = @current_hp - hp
     if @current_hp <= 0
       self.die
@@ -31,7 +31,16 @@ def self.count
   end
 
   def die
-    puts "#{@species}, the species has died!"
+    puts "Oh no! the #{@species}, has died!"
   end
-
 end
+
+  class NPC < Being #start the class in class
+    @name = opts[:name]
+    @professions = opts[:professions] || ["fighter", "thief", "wizard", "ranger", "baker", "butcher", "candlestick maker"].sample
+    @bankroll = opts[:bankroll] || @@random.rand(10..20)
+    @inventory = opts[:inventory] || []
+    super
+  end
+end #end class in a class
+#I did a lot of video watching. Online reading, and asking questions. Also I have a book on Ruby I am reading. This took me hours to get up to. Though it's not completed all the way. I feel happy that I have this a shot.
