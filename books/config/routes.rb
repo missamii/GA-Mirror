4452a133a0800573c874b004fba29ba7a30a3367
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-08-22 22:18:41 -0400

    testing found typos postman

--- books/config/routes.rb
+++ books/config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  get "/book"                              => "book#index"
-  get "/book/:id"                          => "book#show"
-  get "/book/by-title/:title"              => "book#title"
-  get "/book/by-author/:author"            => "book#author"
-  get "/book/by-genre/:genre"              => "book#genre"
-  get "/book/by-year-of-publication/:year" => "book#year"
-  get "/book/by-publisher/:publisher"      => "book#publisher"
-  post "/book/new"                         => "book#create"
-  delete "/book/:id"                       => "book#destroy"
-  put "/book/:id"                          => "book#update"
+  get "/books"                              => "book#index"
+  get "/books/:id"                          => "book#show"
+  get "/books/by-title/:title"              => "book#title"
+  get "/books/by-author/:author"            => "book#author"
+  get "/books/by-genre/:genre"              => "book#genre"
+  get "/books/by-year-of-publication/:year" => "book#year"
+  get "/books/by-publisher/:publisher"      => "book#publisher"
+  post "/books/new"                         => "book#create"
+  delete "/books/:id"                       => "book#destroy"
+  put "/books/:id"                          => "book#update"
 
 end
