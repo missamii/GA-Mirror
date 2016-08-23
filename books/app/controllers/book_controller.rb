4a887fee255da09ba576dcd1507793826d1334a6
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-08-22 21:17:33 -0400

    controller

--- books/app/controllers/book_controller.rb
+++ books/app/controllers/book_controller.rb
@@ -1,2 +1,77 @@
 class BookController < ApplicationController
+
+  api_results = [
+  {
+    id: 1,
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "JK Rowling",
+    year: 1997,
+    genres: [
+      "children",
+      "fantasy"
+    ],
+    publisher: "Scholastic"
+  },
+  {
+    id: 2,
+    title: "Profiles in Courage",
+    author: "John F. Kennedy",
+    year: 1957,
+    genres: [
+      "biography",
+      "politics"
+    ]
+    publisher: "Harper & Brothers"
+  },
+  {
+    id: 3,
+    title: "Dune",
+    author: "Frank Herbert",
+    year: 1965,
+    genres: [
+      "science fiction"
+    ],
+    publisher: "Arrakis Press"
+  },
+  {
+    id: 4,
+    title: "A Brilliant Book",
+    author: "Ethan Friedman",
+    year: 2017,
+    genres: [
+      "science fiction",
+      "politics"
+    ],
+    publisher: "Scholastic"
+  },
+  {
+    id: 5,
+    title: "Liza's pretty good book",
+    author: "Liza Ramo",
+    year: 1965,
+    publisher: "HarperPenguinRandomHouse"
+    genres: [
+      "biography",
+      "childrens"
+    ]
+  },
+  {
+    id: 6,
+    title: "Syed's also pretty okay book",
+    author: "Syed Salahuddin",
+    year: 1997,
+    publisher: "Super Syed-an Books"
+    genres: [
+      "fantasy",
+      "biography",
+      "reference",
+      "cooking"
+    ]
+  }
+]
+
+def index
+  render json: @@data
+end
+
 end
--- books/config/routes.rb
+++ books/config/routes.rb
@@ -1,25 +1,14 @@
 Rails.application.routes.draw do
 
-  # Book Routes
-    # all books
-    get "/books"                                => "books#index"
-    # will fetch a book by id
-    get "/books/:id"                            => "books#book_by_id"
-    # will send the book with that title.
-    get "/books/by-title/:title"                => "books#book_by_title"
-    # will send the book by that author
-    get "/books/by-author/:author"              => "books#book_by_author"
-    # will send all books of that genre
-    get "/books/by-genre/:genre"                => "books#book_by_genre"
-    # will send all books published in the given year
-    get "/books/by-year-of-publication/:year"   => "books#book_by_year"
-    # will send all books published by a given publishing house
-    get "/books/by-publisher/:publisher"        => "books#book_by_publisher"
-    # will add a new book to your array of sample data (see below)
-    post "/books/new"                           => "books#create"
-    # will delete that book
-    delete "/books/:id"                         => "books#destroy"
-    # will update that book
-    put "/books/:id"                            => "books#update"
+  get "/book"                              => "book#index"
+  get "/book/:id"                          => "book#show"
+  get "/book/by-title/:title"              => "book#title"
+  get "/book/by-author/:author"            => "book#author"
+  get "/book/by-genre/:genre"              => "book#genre"
+  get "/book/by-year-of-publication/:year" => "book#year"
+  get "/book/by-publisher/:publisher"      => "book#publisher"
+  post "/book/new"                         => "book#create"
+  delete "/book/:id"                       => "book#destroy"
+  put "/book/:id"                          => "book#update"
 
 end
